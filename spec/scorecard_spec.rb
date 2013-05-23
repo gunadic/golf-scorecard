da46aabbcbd7984c6cb0383f3d04511c5b5999ad
Samuel McTaggart <sam.mctaggart@gmail.com>
2013-05-23 15:54:38 -0400

    all 3 parts complete

--- spec/scorecard_spec.rb
+++ spec/scorecard_spec.rb
@@ -1,4 +1,5 @@
 require 'rspec'
+require 'pry'
 require_relative '../lib/course'
 
 describe HoleLayout do
@@ -25,6 +26,7 @@ end
 
 describe ScoreCard do
   let(:hole) {HoleLayout.new.load_course}
+  let(:ex_score) {ScoreCard.new(hole)}
   it "receives an array containing the course layout" do
     scores = ScoreCard.new(hole)
     expect(scores.course_score).to eql(hole)
@@ -35,4 +37,32 @@ describe ScoreCard do
     test_score.load_scores
     expect(test_score.scores.length).to_not eql(0)
   end
+
+  it 'compares par to the golfers score' do
+    ex_score.load_scores
+    woods_score = ex_score.scores["Woods, Tiger"]
+    expect(woods_score.length).to eql(ex_score.course_score.length)
+  end
+
+  it 'converts difference between score and par to golf terms' do
+    ex_score.load_scores
+    ex_score.convert
+    woods_score = ex_score.wordified_scores[0]
+    expect(woods_score[0].class).to eql(String)
+  end
+
+  it 'tabulates a golfer\'s total score' do
+    ex_score.load_scores
+    ex_score.total
+    expect(ex_score.total_scores[0]).to eql(63)
+  end
+
+  it 'prints out the player\'s scorecard all pretty-like' do
+    ex_score.load_scores
+    ex_score.convert
+    ex_score.total
+    ex_score.output
+    expect(ex_score.outputs[0].class).to eql(String)
+  end
+
 end
